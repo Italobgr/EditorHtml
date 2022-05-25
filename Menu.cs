a65e36fd4589eb0fb0657aa17eee2f00c14bf794
Italubgr <italobrunobgr@gmail.com>
2022-05-25 20:24:32 -0300

    some init msg

---
+++ Menu.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace EditorHtml
+{
+
+
+    public static class Menu
+    {
+        public static void Show()
+        {
+
+            Console.Clear();
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.ForegroundColor = ConsoleColor.Black;
+            DrawScreen();
+    
+
+    }
+
+        public static void DrawScreen()
+        {
+
+            Console.Write("+");
+            for (int i = 0; i <= 30; i++)
+            {
+                Console.Write("-");
+            }
+            Console.Write("+");
+            Console.Write("\n");
+
+            for (int lines = 0;lines <=10; lines++)
+            {
+                Console.Write("|");//esta errado depois eu corrijo
+            }
+
+        }
+    }
+
+}
